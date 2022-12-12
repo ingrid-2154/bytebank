52619dc038cf618fdd7650ffeddf19b1a2b9ba72
ingrid-2154 <ingrid_leite2154@hotmail.com>
2022-12-12 19:40:47 -0300

    mensagem

---
+++ src/TesteReferencia.java
@@ -0,0 +1,20 @@
+
+public class TesteReferencia {
+ public static void main(String[] args) {
+	Conta primeiraConta = new Conta();
+	primeiraConta.saldo = 300;
+	
+	System.out.println("saldo primeira conta " + primeiraConta.saldo);
+ 
+	Conta segundaConta = primeiraConta;
+	
+	System.out.println("saldo segunda conta " + segundaConta.saldo);
+ 
+	segundaConta.saldo += 100;
+	System.out.println(primeiraConta.saldo);
+ 
+	if(primeiraConta == segundaConta) {
+	System.out.println("mesma conta");
+		}
+ 	}
+ 	}
